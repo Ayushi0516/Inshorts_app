7cd153f0e91d265b43f64f4eb720caff61567c7c
Ayushi0516 <ayushigupta0516@gmail.com>
2023-09-14 19:47:07 +0530

    news mapped and routes added

---
+++ Screens/DiscoverScreen.js
@@ -0,0 +1,60 @@
+import { View, Text,StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function DiscoverScreen() {
+  return (
+    <View style={styles.discover}>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  discover: {
+    padding: 10,
+    alignItems: "center",
+  },
+  subtitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    paddingBottom: 8,
+    marginHorizontal: 5,
+    borderBottomColor: "#007FFF",
+    borderBottomWidth: 5,
+    alignSelf: "flex-start",
+    borderRadius: 10,
+  },
+  category: {
+    height: 130,
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  categoryImage: {
+    height: "60%",
+    width: "100%",
+    resizeMode: "contain",
+  },
+  name: {
+    fontSize: 14,
+    textTransform: "capitalize",
+  },
+  sources: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    paddingVertical: 15,
+  },
+  sourceContainer: {
+    height: 150,
+    width: "40%",
+    borderRadius: 10,
+    margin: 15,
+    backgroundColor: "#cc313d",
+  },
+  sourceImage: {
+    height: "100%",
+    borderRadius: 10,
+    resizeMode: "cover",
+  },
+});
